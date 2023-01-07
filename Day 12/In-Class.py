9c839f06d44e9d8af538f9933978452394e555fd
unknown <davidoadeleke90@gmail.com>
2023-01-07 08:20:04 +0100

    Another one

---
+++ Day 12/In-Class.py
@@ -0,0 +1,58 @@
+''' Lesson 1 - Namespace: Local vs. Global '''
+
+enemies = 1
+def increase_enemies():
+	enemies = 2 
+	print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+# # # Local Scope
+# # def drink_potion():
+# # 	potion_strength = 2
+# # 	print(potion_strength)
+
+# # drink_potion
+# # print(potion_strength) # Name error
+
+# Global Scope
+player_health = 10
+
+def game():
+	def drink_potion():
+		potion_strength = 2
+		print(player_health)
+	drink_potion()
+
+
+
+# There is no block scope
+
+game_level = 3
+def create_enemy():
+	enemies = ["Skeleton", "Zombie", "Alien"]
+	if game_level < 5:
+		new_enemy = enemies[0]
+
+	print(new_enemy)
+
+
+# Modifying Global Scope
+
+	
+enemies = 1
+def increase_enemies():
+	global enemies # Always try to avoid.
+	enemies += 2
+	print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+
+#Global Constants
+
+PI = 3.14159
+URL = "https://www.google.com"
+TWITTER_HANDLE = "@TB_David001"
---
+++ Day 12/Project.py
@@ -0,0 +1,46 @@
+from random import randint
+from art import logo
+from replit import clear
+
+def run_game():
+	print(logo)
+	print("Welcome to the Number Guessing Game!")
+	print("I'm thinking of a number between 1 and 100.")
+	level_choice = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+	if level_choice == 'easy':
+		attempts = 10
+	elif level_choice == 'hard':
+		attempts = 5
+	else:
+		print("You have entered an invalid input.")
+	
+	computer_choice = randint(1, 101)
+	game_end = False
+	while not game_end:
+		if attempts > 0:
+			print(f"You have {attempts} attempt(s) remaining to guess the number.")
+			user_guess = int(input("Make a guess: "))
+			if user_guess == computer_choice:
+				print(f"You got it! The answer was {computer_choice}")
+				game_end = True
+			elif user_guess > computer_choice:
+				print("Too high.")
+				if attempts > 1:
+					print("Guess again.")
+				attempts -= 1
+			elif user_guess < computer_choice:
+				print("Too low.")
+				if attempts > 1:
+					print("Guess again.")
+				attempts -= 1
+		if attempts == 0:
+			game_end = True
+			print(f"You've run out of guesses, answer was {computer_choice}, you lose.")
+			
+		if (game_end == True) or (attempts == 0):
+			replay = input("Do you want to play again? Type 'yes' or 'no': ").lower()
+			if replay == 'yes':
+				clear()
+				run_game()
+			game_end = True
+run_game()
