d925fa7f6381c00394946d56b149ec9f4498b720
unknown <davidoadeleke90@gmail.com>
2023-03-18 06:33:36 +0100

    Another One

---
+++ Day 66/Building Your Own API with RESTful Routing/Cafe API/main.py
@@ -0,0 +1,81 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from random import choice
+
+app = Flask(__name__)
+
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/random")
+def random():
+    cafe_database = db.session.query(Cafe).all()
+    random_cafe = choice(cafe_database)
+    return jsonify(cafe={
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price,
+        "has_sockets": random_cafe.has_sockets,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "id": random_cafe.id,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "map_url": random_cafe.map_url,
+        "name": random_cafe.name,
+        "seats": random_cafe.seats
+    })
+
+
+@app.route("/all")
+def all_cafes():
+    cafes = db.session.query(Cafe).all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+
+
+@app.route("/search")
+def search():
+    query_location = request.args.get("loc")
+    cafes = Cafe.query.filter_by(location=query_location).all()
+    if cafes is None or cafes == []:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+    else:
+        return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+
+
+# HTTP GET - Read Record
+
+# HTTP POST - Create Record
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
